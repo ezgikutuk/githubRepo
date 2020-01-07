02dd1f55f2aa11ad5c68878a67af91884fb90dd3
Ezgi Kütük <ezgi.kutuk@derinarge.com>
2020-01-07 16:31:09 +0300

    main dosyaları

---
+++ main.js
@@ -0,0 +1,44 @@
+// tslint:disable-next-line:no-shadowed-variable
+// TypeScript
+// function log(message) {
+//     console.log(message);
+//   }
+//   let message = 'Hello World';
+//   log(message);
+// Declaring Variables
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally: ' + i);
+}
+doSomething();
+// /*let a: number;
+// let b: boolean;
+// let c: string;
+// let d: any;
+// let e: number[] = [1, 2, 3];
+// let f: any[] = [1, true, 'a', false];
+// const ColorRed = 0;
+// const ColorGreen = 1;
+// const ColorBlue = 2;
+// enum Color {Red, Green, Blue};
+// let backgroundColor = Color.Red;*/
+// let message;
+// message = 'abc';
+// //let  alternativeWay = (message as string).endsWith('c');
+// /*let message;
+// message = 'abc';
+// let  alternativeWay = message.endsWith('c');
+// */
+// interface Point{
+//     x: number,
+//     y: number
+// }
+// let drawPoint = (point:Point  ) =>{
+//     //
+// }
+// drawPoint({
+//     x: 1,
+//     y: 2
+// })
